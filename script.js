6af416a320ca26ab5fb3fe87afac4a77273878f2
Homelander <60612488+nithinsgr@users.noreply.github.com>
2023-05-06 09:07:40 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-var headers = ["Discover the Wonders of the Universe", "Explore the Planets with NASA Images", "Journey Through the Cosmos", "Explore Wonders of Space through NASA's Vast Gallery of Images", "Images from NASA Gallery"];
-var header_index = 0;
-var header_elem = $("#header");
-setInterval(function() {
-    header_elem.text(headers[header_index]);
-    header_index = (header_index + 1) % headers.length;
-}, 5000);
