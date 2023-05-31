8a4d490624b563c5852af4f6c8795e671f7f6c47
nithin <nithin.sagar.359@gmail.com>
2023-05-31 19:43:46 +0530

    added about dialog to every page.

--- js/script.js
+++ js/script.js
@@ -6,25 +6,3 @@ setInterval(function() {
     header_index = (header_index + 1) % headers.length;
 }, 5000);
 
-const dialog = document.getElementById('dialogBtn');
-const about = document.getElementById('about');
-const btn = document.getElementById('btn');
-
-dialog.addEventListener('click', function () {
-    document.body.style.filter = "blur(5px)";
-    about.showModal();
-
-    // setTimeout(function () {
-    //     document.body.style.filter = "none";
-    //     about.close();
-    // }, 5000); 
-});
-
-document.addEventListener('click', function (event) {
-    if (event.target === btn || (event.target !== dialog && !dialog.contains(event.target))) {
-        document.body.style.filter = "none";
-        about.close();
-    }
-});
-
-
